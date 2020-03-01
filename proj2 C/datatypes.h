49a0c46c6bba66fb7b5c5ffab6dd58ded7d09817
Boykoa97 <43189132+Boykoa97@users.noreply.github.com>
2020-02-29 17:33:22 -0800

    Delete datatypes.h

--- proj2 C/datatypes.h
+++
@@ -1,9 +0,0 @@
-// this file is for linking the different objects
-// such as tasks, queue, master and slaves
-
-typedef struct Task {
-  
-  int id; // id number of the task
-  int length; // length of task in seconds
-
-} Task;
