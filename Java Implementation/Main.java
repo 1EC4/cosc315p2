763e73b658e55c00f7dc2b6545523f0bf68456f7
1EC4 <nrieske@alumni.ubc.ca>
2020-03-03 20:40:24 -0800

    Added command line argument support

--- Java Implementation/Main.java
+++ Java Implementation/Main.java
@@ -9,17 +9,50 @@ import java.time.format.DateTimeFormatter;
 public class Main {
 
 	// Specifies number of slaves (must be > 0)
-	private static final int NUM_SLAVES = 5;
+	private static int NUM_SLAVES = 5;
 	// Specifies maximum task length (in seconds)
-	private static final int MAX_TASK_LEN = 20;
+	private static int MAX_TASK_LEN = 20;
 	// Specifies maximum idle time between generating tasks of master thread (in seconds)
-	private static final int MAX_MASTER_IDLE = 3;
+	private static int MAX_MASTER_IDLE = 3;
 	// Specifies maximum running time of program (0 = no maximum) in milliseconds
 	private static final int TIMEOUT = 1800000; // 1800000 milliseconds = 30 minutes
 	// Format for time debug output
 	private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm:ss");
 	
 	public static void main(String[] args) throws Exception {
+
+		if (args.length > 0){
+			try {
+				NUM_SLAVES = Integer.parseInt(args[0]);
+			} catch (Exception e){
+				System.out.printf("Bad Syntax. Expected: java Main [num_slaves] [max_task_len] [max_master_idle]%n");
+				return;
+			}
+		}
+		
+		if (args.length > 1){
+			try {
+				MAX_TASK_LEN = Integer.parseInt(args[1]);
+			} catch (Exception e){
+				System.out.printf("Bad Syntax. Expected: java Main [num_slaves] [max_task_len] [max_master_idle]%n");
+				return;
+			}
+		}
+
+		if (args.length == 3){
+			try {
+				MAX_MASTER_IDLE = Integer.parseInt(args[2]);
+			} catch (Exception e){
+				System.out.printf("Bad Syntax. Expected: java Main [num_slaves] [max_task_len] [max_master_idle]%n");
+				return;
+			}
+		}
+
+		if (args.length > 3){
+			System.out.printf("Bad Syntax. Expected: java Main [num_slaves] [max_task_len] [max_master_idle]%n");
+			return;
+		}
+
 		SafeQueue<Task> taskQ = new SafeQueue<Task>(NUM_SLAVES);
 		
 		Thread master = new Thread(new Master(MAX_TASK_LEN, MAX_MASTER_IDLE, NUM_SLAVES, taskQ));
